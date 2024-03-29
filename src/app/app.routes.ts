fe5082e6432ce2c9ad0265fef7e8906a066ee9a1
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-03-29 23:24:21 +0500

    feat: add pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {MainPageComponent} from './pages/main-page/main-page.component';
+import {NotFoundPageComponent} from './pages/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'app',
+    pathMatch: 'full'
+  },
+  {
+    path: 'app',
+    component: MainPageComponent
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent
+  }
+];
