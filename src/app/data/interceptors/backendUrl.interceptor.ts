eb2e61dd170e197ed0db61034011c81842c9a365
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-03-30 12:48:43 +0500

    feat: add backend-token and interceptor

---
+++ src/app/data/interceptors/backendUrl.interceptor.ts
@@ -0,0 +1,11 @@
+import { HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
+import { BACKEND_URL_TOKEN } from '../tokens/backend-url.token';
+
+export function backendUrlInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+  const backendUrl: string = inject(BACKEND_URL_TOKEN);
+  const apiReq: HttpRequest<any> = req.clone({ url: `${backendUrl}/${req.url}` });
+
+  return next(apiReq);
+}
---
+++ src/app/data/tokens/backend-url.token.ts
@@ -0,0 +1,8 @@
+import { InjectionToken } from '@angular/core';
+
+export const BACKEND_URL_TOKEN: InjectionToken<string> = new InjectionToken<string>(
+  'url for making api requests',
+  {
+    factory: () => 'http://localhost:3000'
+  }
+);
