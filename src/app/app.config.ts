d30f970c9535b404e015c95ceca24fd135884c03
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-03-31 23:54:58 +0500

    feat: add taiga-ui

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { TuiRootModule } from "@taiga-ui/core";
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {backendUrlInterceptor} from './data/interceptors/backendUrl.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideHttpClient(
       withInterceptors([
         backendUrlInterceptor
       ])
     ),
-    provideRouter(routes)
+    provideRouter(routes),
+    importProvidersFrom(TuiRootModule)
   ]
 };
--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,10 +1,11 @@
+import { TuiRootModule, TuiAlertModule } from "@taiga-ui/core";
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRootModule, TuiAlertModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
