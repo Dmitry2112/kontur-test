7639eb6161c635944073b149cfee66a14edb5ea4
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-03-31 23:38:08 +0500

    feat: add filters component

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,11 +1,13 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {HotelsComponent} from '../../components/hotels/hotels.component';
+import {FiltersComponent} from '../../components/filters/filters.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
   imports: [
-    HotelsComponent
+    HotelsComponent,
+    FiltersComponent
   ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
